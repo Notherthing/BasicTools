cca57a9f4700b6816257c0dca9f66cdf61bf258f
Nothering <1040986768@qq.com>
2022-10-16 14:50:41 +0800

    10.16 basic thread addition done

---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/thread/thread.cpp.o"
+  "CMakeFiles/test.dir/src/thread/thread.cpp.o.d"
+  "CMakeFiles/test.dir/test/thread_test.cpp.o"
+  "CMakeFiles/test.dir/test/thread_test.cpp.o.d"
+  "bin/test"
+  "bin/test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/threadtest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for threadtest.
+# This may be replaced when dependencies are built.
